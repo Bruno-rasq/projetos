877e8013e2382297735269801e34bd5b08f15192
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-02-15 18:49:59 -0200

    att

--- piano-virtual/javascript/script.js
+++ piano-virtual/javascript/script.js
@@ -1,8 +1,15 @@
 const keys = document.querySelectorAll('.key');
 
+/*variavel array criado para capturar as teclas utilizadas no piano */
+
+const presskey = (key) => {
+    key.style,background = '#ddd'
+    /*assi, que a tecla for pressionada haverá uma mudança de cor */
+}
+
 keys.forEach((key) => {
 
-    key.addEventListener('click', () => console.log('clicou'))
+    key.addEventListener('click', () => presskey(key))
 
     /* seção utilizada para percorrer a array keys e add um addeventlistener em cada tecla do piano.*/ 
 });
