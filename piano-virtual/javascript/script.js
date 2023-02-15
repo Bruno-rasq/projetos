ea6ae45b1ccbb333bdefb88fb9203e65dfcc5523
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-02-15 18:15:10 -0200

    inicializando script

---
+++ piano-virtual/javascript/script.js
@@ -0,0 +1,8 @@
+const keys = document.querySelectorAll('.key');
+
+keys.forEach((key) => {
+
+    key.addEventListener('click', () => console.log('clicou'))
+
+    /* seção utilizada para percorrer a array keys e add um addeventlistener em cada tecla do piano.*/ 
+});
